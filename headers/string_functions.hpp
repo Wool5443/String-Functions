8f4d6a00f8af1a1f552b90c6a9a41b4a76ab1ce7
Wool5443 <mihsolodilov2015@gmail.com>
2023-08-25 17:46:50 +0300

    Header

---
+++ headers/string_functions.hpp
@@ -0,0 +1,7 @@
+size_t StringLength(const char* string);
+
+char* StringCopy(char* destination, const char* source, size_t maxLength);
+
+char* StringCopyAll(char* destination, const char* source);
+
+bool StringCompare(const char* s1, const char* s2);
