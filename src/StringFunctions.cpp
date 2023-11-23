1014d9fd512d4beedffb6003534d167692e6946b
Wool5443 <mihsolodilov2015@gmail.com>
2023-11-23 11:08:57 +0300

    Upd old libs

--- src/StringFunctions.cpp
+++ src/StringFunctions.cpp
@@ -302,7 +302,7 @@ int StringIsEmptyChars(const char* string, char terminator)
     while (isspace(*string) && *string != terminator)
         string++;
 
-    if (*string == 0 || *string == terminator)
+    if (*string == terminator)
         return 1;
     return 0;
 }
